9ee279aa9f434d422e23a6e8e0705dfbba14e927
naechrist <729jenae.christensen@gmail.com>
2021-04-23 11:40:29 -0400

    SessionsController

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,3 +1,35 @@
 class SessionsController < ApplicationController
 
+    def welcome
+    end
+
+    def destroy
+        session.delete(:user_id)
+        redirect_to '/'
+    end
+
+    def new
+    end
+
+    def create
+        if params[:provider]
+            @user = User.create_by_google_omniauth(auth)
+            session[:user_id] = @user.id 
+            redirect_to user_path(@user)
+        else
+            @user  User.find_by(username: params[:user][:username])
+            if @user && @user.authenticate(params[:user][:password])
+                session[:user_id] = @user.id 
+                redirect_to user_path(@user)
+            else 
+                flash[:error] = "Incorrect info, please try again."
+                redirect_to login_path
+            end
+        end
+    end
+
+    private
+    def auth
+        request.env['omniauth.auth']
+    end
 end
